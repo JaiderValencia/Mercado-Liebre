e0faacc382feb29ec9ec603d242da4ca4240341c
JaiderValencia <janvame05@gmail.com>
2023-01-31 20:45:28 -0500

    Agrego el register en el node

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"));
 })
 
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
+})
+
 app.listen(port, () => {
     console.log(`Conectado en el puerto ${port}`);
 })
