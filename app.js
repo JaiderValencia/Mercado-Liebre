99549bcaf5dd6ad959b0905ee7ba0851d47067ea
JaiderValencia <janvame05@gmail.com>
2023-02-07 21:52:22 -0500

    Agrego el puerto de produccion

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const publicPath = path.join(__dirname, "/public");
 
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
